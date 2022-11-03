d48ee063bb794644569b67ced0c864301927dfa2
Сушкевич Иван Дмитриевич <ivan.sushkevich@softclub.by>
2022-11-03 12:11:12 +0300

    Add about page, footer, routing

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Task} from '../../Task';
+import {UiService} from '../../services/ui.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -12,8 +14,11 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() {
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
   }
 
   ngOnInit(): void {
